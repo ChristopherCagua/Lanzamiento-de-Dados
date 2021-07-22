eb6eb3b8d2ab135fda09af8a160e9ef0a04fb28d
ChristopherCagua <caguachristopher@gmail.com>
2021-07-21 21:19:31 -0500

    Lanzamiento de dados

---
+++ cmake-build-debug/CMakeFiles/Juego_de_DADOS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Juego_de_DADOS.dir/main.cpp.obj"
+  "Juego_de_DADOS.exe"
+  "Juego_de_DADOS.exe.manifest"
+  "Juego_de_DADOS.pdb"
+  "libJuego_de_DADOS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Juego_de_DADOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
